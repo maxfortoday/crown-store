2cb9819e426a7b934596f9815503c9bd2ba51f4b
maxim <admtrigger@gmail.com>
2020-06-17 17:59:08 +0300

    mobile friendly

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,6 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
@@ -12,9 +10,10 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
+import {GlobalStyle} from './global.styles';
+
 import { selectCurrentUser } from './redux/user/user.selectors';
 import {checkUserSession} from './redux/user/user.actions';
-
 const App = ({checkUserSession, currentUser}) => {
 
   useEffect(() => {
@@ -23,23 +22,24 @@ const App = ({checkUserSession, currentUser}) => {
 
     return (
       <div>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-          <Route
-            exact
-            path='/signin'
-            render={() =>
-              currentUser ? (
-                <Redirect to='/' />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            }
-          />
-        </Switch>
+        <GlobalStyle/>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={ShopPage} />
+            <Route exact path='/checkout' component={CheckoutPage} />
+            <Route
+              exact
+              path='/signin'
+              render={() =>
+                currentUser ? (
+                  <Redirect to='/' />
+                ) : (
+                  <SignInAndSignUpPage />
+                )
+              }
+            />
+          </Switch>
       </div>
     );
   }
--- client/src/components/sign-up/sign-up.styles.jsx
+++ client/src/components/sign-up/sign-up.styles.jsx
@@ -4,6 +4,9 @@ export const SignUpContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 380px;
+  @media screen and (max-width: 800px) {
+    width: unset;
+  }
 `;
 
 export const SignUpTitle = styled.h2`
